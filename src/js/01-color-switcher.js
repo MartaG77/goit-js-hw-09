0fdbe6f39a96f31d8a757a3cbb853ce9b145df96
MartaG77 <129276573+MartaG77@users.noreply.github.com>
2023-10-08 10:33:37 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+const changeColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+startBtn.addEventListener('click',() => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    changeColor();
+  }, 1000);
+});
+stopBtn.addEventListener ('click',() => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+});
+stopBtn.disabled = true;
